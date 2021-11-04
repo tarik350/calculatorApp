089f2b814b52c5c958a49705d7f7d844d5d6c7a9
tarik350 <tarikteshome3333@gmail.com>
2021-11-04 07:14:42 -0700

    added the toggle button and modified some styles

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-console.log("hello world!")
+const toggleBox =document.querySelector('.toggle-box');
+const circle=document.querySelector('.circle')
+
+toggleBox.addEventListener("click", ()=>{
+    circle.classList.toggle("circle-position2");
+    let bool=circle.classList.contains('circle-position2')
+
+    if(bool){
+       toggleBox.addEventListener("click",()=>{
+            circle.classList.toggle('.circle-position3');
+            let bool=circle.classList.contains('.circle-position3');
+            if(bool){
+                circle.style.left="3em";
+            }
+            else{
+                circle.style.left="0px";
+            }
+
+       })
+
+    }
+})
